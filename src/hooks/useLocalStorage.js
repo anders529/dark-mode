3ace06d055f97c997147d60ecdd12b253cab1001
Aaron Andersen <anderseana@tutanota.com>
2020-01-15 16:47:29 -0600

    useLocalStorage now working

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,18 +1,18 @@
 import {useState} from 'react';
 
-export const useLocalStorage = (DarkMode, setDarkMode) => {
+export const useLocalStorage = (DarkMode, setDarkMode, key) => {
     const [storedValue, setStoredValue] = useState(() => {
         if (localStorage.getItem(key)) {
             return JSON.parse(localStorage.getItem(key));
         }
         else {
-            localStorage.setItem(key, JSON.stringify(initialValue));
-            return initialValue;
+            localStorage.setItem(key, JSON.stringify(setDarkMode));
+            return setDarkMode;
         }
     });
     const setValue = newValue => {
         setStoredValue(newValue);
-        localStorage.setItem(key, JSON.stringify(initialValue));
+        localStorage.setItem(key, JSON.stringify(setDarkMode));
     };
     return [storedValue, setValue];
 };
